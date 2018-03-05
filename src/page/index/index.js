8bb5a30788c1b207e055879f945399c231ac8dc4
riccin <pengcheng9101@outlook.com>
2018-03-05 22:40:37 +0800

    工具类

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
 * @Author: pengcheng9101
 * @Date:   2018-02-09 21:48:09
 * @Last Modified by:   riccin
-* @Last Modified time: 2018-03-04 22:08:10
+* @Last Modified time: 2018-03-05 21:53:10
 */
 'use strict';
 // var $$ = require('jquery');
@@ -10,16 +10,23 @@
 // $$('body').html('index hollo~~~ZSS');   // 
 // require('./index.css');
 // require('../module.js');
-var _net = require('/util/net.js');
+var _net = require('util/net.js');
 
-_net.request({
-   url     : 'http://happymmall.com/product/list.do?keyword=1',
-   success :  function(res){
-        console.log(res);
-   },
-   error   :   function(errMsg){
-         console.log(errMsg);
+// _net.request({
+//    url     : 'http://localhost:8088/product/list.do?keyword=1',
+//    success :  function(res){
+//         console.log(res);
+//    },
+//    error   :   function(errMsg){
+//          console.log(errMsg);
 
-   }
+//    }
 
-});
+// });
+
+//console.log(_net.getUrlParam('test'));
+var html = '<div>{{data}}</div>';
+var data = {
+    data : 123
+}
+ console.log(_net.renderHtml(html,data));
--- src/util/net.js
+++ src/util/net.js
@@ -2,10 +2,14 @@
 * @Author: riccin
 * @Date:   2018-03-04 20:52:53
 * @Last Modified by:   riccin
-* @Last Modified time: 2018-03-04 22:05:26
+* @Last Modified time: 2018-03-05 22:06:30
 webpackJson
 */
 'use strict';
+var Hogan = require('hogan');
+var config = {
+    serverHost : ''
+};
 var _net = {
 
     request : function(param){
@@ -35,8 +39,53 @@ var _net = {
         });
 
     },
+    // 获取服务器地址
+    getServerUrl : function(path){
+        return conf.serverHost + path;
+    },
+    //获取url 参数   keyword=xxx&page=1
+    getUrlParam : function(name){
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+        var result =window.location.search.substr(1).match(reg);
+        return result?decodeURIComponent(result[2]) : null;
+    },
+    renderHtml : function(htmlTemplate,data){
+            var template = Hogan.compile(htmlTemplate),
+            result   = template.render(data);
+            return result;
+
+    },
+    successTips : function(msg){
+            alert(msg || '操作成功');
+    },
+    errerTips   : function(msg){
+            alert(msg || '操作失败');
+
+    },
+    // 字段验证, 支持非空判断 验证手机邮箱 
+    validata    : function(value,type){
+            var value = $.trim(value);
+            // 非空验证
+            if ('require' === type) {
+                return !!value;
+            }
+            // 手机号验证
+            if ('phone' === type) {
+                return /^1\d{10}$/.test(value);
+            }
+            // 邮箱验证
+            if ('mail' === type) {
+                return [a-zA-Z0-9_-]+ .test(vaule);
+            }
+    },
+
+    // 统一登录处理
     doLogin   : function(){
                 window.location.href = './login.html?redirect='+encodeURIComponent(window.location.herf);
+    }
+    // 跳转首页
+    goHome    : function(){
+        window.location.href = './index.html';
     }
 };
 module.exports = _net;
